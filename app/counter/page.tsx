98a9f46887ef61723a8f88a31e78cf77d9b333b9
Brian Emilius <be@rts.dk>
2025-11-19 13:55:31 +0100

    add counter page

---
+++ app/counter/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react';
+
+export default function Counter() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div>
+			<h1>Counter: {count}</h1>
+			<button onClick={() => setCount(count + 1)}>+1</button>
+		</div>
+	);
+}
